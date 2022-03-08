b579f92b8f068187268ebd8030644e819535b7a7
mofon <immofon@163.com>
2022-03-09 00:52:29 +0800

    update

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"net/http"
 	"os"
 )
 
 func main() {
+	fmt.Println(os.Getenv("TOKEN"))
 	resp, err := http.Get("https://en.wikipedia.org/wiki/Main_Page")
 	if err != nil {
 		panic(err)
